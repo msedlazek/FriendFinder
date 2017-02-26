947e2cc7f56897a96c25bffac7e55e909a070638
msedlazek <mark.sedlazek@gmail.com>
2017-02-26 14:26:18 -0500

    Started html, reviewing code.

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var port = 3000;
+
+var app = express();
+
+// WEEK 13 SATURDAY CLASS VIDEO
